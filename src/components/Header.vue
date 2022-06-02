3a015196888427ac041b08c745ef71b139ff28bb
Your Name <mnabale@mykloud.io>
2022-06-02 21:46:33 +0300

    aa

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,44 @@
 <template>
-  <v-toolbar class="ma-auto" flat color="heder_footer" width="100%">
-    <v-toolbar-title>
-      <div class="">
-        <img
-          @click="goHome"
-          style="cursor: pointer"
-          class="logo"
-          src="https://cdn.mos.cms.futurecdn.net/m5ADpuAdKTpadnUWmsCrHJ.jpg"
-          width="50px"
-        /></div
-    ></v-toolbar-title>
-    <v-spacer></v-spacer>
+  <div style="width: 100%">
+    <v-toolbar class="ma-auto fixedNavbar" flat color="heder_footer">
+      <v-toolbar-title>
+        <div class="">
+          <img
+            @click="goHome"
+            style="cursor: pointer"
+            class="logo"
+            src="https://cdn.mos.cms.futurecdn.net/m5ADpuAdKTpadnUWmsCrHJ.jpg"
+            width="50px"
+          /></div
+      ></v-toolbar-title>
+      <v-spacer></v-spacer>
 
-    <v-toolbar-items class="">
-      <v-btn text @click="scroll('hussein-award')">
-        جائزة الحسين للعمل التطوعي
+      <v-toolbar-items class="">
+        <v-btn text @click="scroll('hussein-award')">
+          جائزة الحسين للعمل التطوعي
+        </v-btn>
+        <v-btn text @click="scroll('volunteer-guide')"> دليل التطوع </v-btn>
+        <v-btn text @click="scroll('charter')">
+          ميثاق العمل التطوعي الاردني
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-spacer></v-spacer>
+
+      <v-btn height="40px" color="secondary" @click="goLogin" class="rounded-0">
+        تسجيل الدخول
       </v-btn>
-      <v-btn text @click="scroll('volunteer-guide')"> دليل التطوع </v-btn>
-      <v-btn text @click="scroll('charter')">
-        ميثاق العمل التطوعي الاردني
-      </v-btn>
-    </v-toolbar-items>
-
-    <v-spacer></v-spacer>
-
-    <v-btn height="40px" color="secondary" @click="goLogin" class="rounded-0">
-      تسجيل الدخول
-    </v-btn>
-  </v-toolbar>
+    </v-toolbar>
+    <v-content class="divClass"></v-content>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HraderComponent",
   methods: {
-    scroll(refName) {
+    async scroll(refName) {
+      if (window.location.pathname !== "/") await this.$router.push("/");
       const element = document.getElementById(refName);
       console.log("refName", refName);
       element.scrollIntoView({ behavior: "smooth" });
@@ -49,4 +53,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.divClass {
+  min-height: 64px;
+  width: 100%;
+}
+.fixedNavbar {
+  position: fixed !important;
+  width: 1400px;
+  min-height: 64px;
+  z-index: 100;
+}
+</style>
